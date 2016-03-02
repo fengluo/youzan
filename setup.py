5845f1fc3049d43a6fe4872d4e4b54c345d59bec
fengluo <fengluo17@gmail.com>
2016-03-02 21:24:38 +0800

    fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='youzan',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/fengluo/youzan',
     license='MIT',
     author='fengluo',
--- youzan/__init__.py
+++ youzan/__init__.py
@@ -16,38 +16,29 @@ class YouZan(object):
         self.format = 'json'
         self.v = '1.0'
         self.sign_method = 'md5'
-        self._arg_dict = {}
 
-    def get_arg_dict(self):
-        self._arg_dict['app_id'] = self.app_id
-        self._arg_dict['format'] = self.format
-        self._arg_dict['sign_method'] = self.sign_method
-        self._arg_dict['v'] = self.v
-        return self._arg_dict
 
-    def set_arg_dict(self, key, value):
-        self._arg_dict[key] = value
-
-    def merge_arg_dict(self, args):
-        self._arg_dict = dict(self._arg_dict, **args)
-
-    def get_sign(self):
+    def get_sign(self, args_dict):
         arg_str = ''.join(
-            ['%s%s' % (key, self.get_arg_dict()[key])
-                for key in sorted(self.get_arg_dict())])
+            ['%s%s' % (key, args_dict[key])
+                for key in sorted(args_dict)])
         arg_str = "{}{}{}".format(self.app_secert, arg_str, self.app_secert)
         return hashlib.md5(arg_str).hexdigest().lower()
 
     def __call__(self, method, **args):
-        self._arg_dict = {}
-        self.method = method
-        self.args = args
-        self.timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.set_arg_dict('method', method)
-        self.set_arg_dict('timestamp', self.timestamp)
-        self.merge_arg_dict(args)
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        payload = self.get_arg_dict()
-        payload['sign'] = self.get_sign()
+        args_dict = {}
+        args_dict['app_id'] = self.app_id
+        args_dict['format'] = self.format
+        args_dict['sign_method'] = self.sign_method
+        args_dict['v'] = self.v
+        args_dict['method'] = method
+        args_dict['timestamp'] = timestamp
+        args_dict.update(args)
+
+        payload = args_dict.copy()
+        payload['sign'] = self.get_sign(args_dict)
+
         r = requests.get(API_BASE, params=payload)
         return r.json()
